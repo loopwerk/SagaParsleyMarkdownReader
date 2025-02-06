9437192d59bad74769f2e8b1f461b871139067a7
Kevin Renskers <kevin@loopwerk.io>
2025-02-06 16:49:59 +0100

    feat: Add a convenience `.parsleyMarkdownReader` property which uses the default function parameters

--- Sources/SagaParsleyMarkdownReader/SagaParsleyMarkdownReader.swift
+++ Sources/SagaParsleyMarkdownReader/SagaParsleyMarkdownReader.swift
@@ -3,6 +3,10 @@ import Saga
 import Parsley
 
 public extension Reader {
+  static var parsleyMarkdownReader: Self {
+    .parsleyMarkdownReader()
+  }
+
   static func parsleyMarkdownReader(
     markdownOptions: MarkdownOptions = [.unsafe, .hardBreaks, .smartQuotes],
     syntaxExtensions: [SyntaxExtension] = SyntaxExtension.defaultExtensions) -> Self {
