9605b14aa43e32eec454d2dff26bcaf95a1a1a93
Kevin Renskers <kevin@loopwerk.io>
2025-02-18 16:41:19 +0100

    Migrate to Saga 2.0.0

--- Sources/SagaParsleyMarkdownReader/SagaParsleyMarkdownReader.swift
+++ Sources/SagaParsleyMarkdownReader/SagaParsleyMarkdownReader.swift
@@ -10,35 +10,14 @@ public extension Reader {
   static func parsleyMarkdownReader(
     markdownOptions: MarkdownOptions = [.unsafe, .hardBreaks, .smartQuotes],
     syntaxExtensions: [SyntaxExtension] = SyntaxExtension.defaultExtensions) -> Self {
-    Reader(supportedExtensions: ["md", "markdown"], convert: { absoluteSource, relativeSource, relativeDestination in
-      let contents: String = try absoluteSource.read()
-
-      // First we parse the markdown file
+    Reader(supportedExtensions: ["md", "markdown"], convert: { absoluteSource in
+      let rawContent: String = try absoluteSource.read()
       let document = try Parsley.parse(
-        contents,
+        rawContent,
         options: markdownOptions,
         syntaxExtensions: syntaxExtensions
       )
-
-      // Then we try to decode the embedded metadata within the markdown (which otherwise is just a [String: String] dict)
-      let decoder = makeMetadataDecoder(for: document.metadata)
-      let published = try resolvePublishingDate(from: absoluteSource, decoder: decoder)
-      let metadata = try M(from: decoder)
-
-      // Create the Item
-      let item = Item(
-        relativeSource: relativeSource,
-        relativeDestination: relativeDestination,
-        title: document.title ?? absoluteSource.lastComponentWithoutExtension,
-        rawContent: contents,
-        body: document.body,
-        published: published,
-        created: absoluteSource.creationDate ?? Date(),
-        lastModified: absoluteSource.modificationDate ?? Date(),
-        metadata: metadata
-      )
-
-      return item
+      return (document.title, document.body, document.metadata)
     })
   }
 }
